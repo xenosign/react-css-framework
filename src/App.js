f66d8faa9ba0a323a41b991013d512941c49d4a6
yurim <dding.yull@gmail.com>
2022-10-19 15:19:40 +0900

    git 연습

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from './pages/Index';
 import Item from './pages/Item';
 import Join from './pages/Join';
 import Login from './pages/Login';
+import Signup1 from './pages/Signup1';
 import AlbumOG from './components/AlbumOg';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/album" element={<AlbumOG />} />
       <Route path="/join" element={<Join />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup1" element={<Signup1 />} />
     </Routes>
   );
 }
